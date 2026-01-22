15cb0d13bd667f342246c78205038d54d4728924
sergey-s-betke <sergey.s.betke@yandex.ru>
2026-01-23 00:45:55 +0300

    feat: add commitlint reusable workflow
    #1

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,16 +1,13 @@
-import type { UserConfig } from '@commitlint/types';
-import { RuleConfigSeverity } from '@commitlint/types';
-
 // eslint-disable-next-line max-len
 // https://github.com/conventional-changelog/commitlint/blob/master/%40commitlint/config-angular/README.md
 // https://github.com/angular/angular/blob/main/CONTRIBUTING.md#commit
 // https://commitlint.js.org/reference/configuration.html
 
-const Configuration: UserConfig = {
+const Configuration = {
 
   extends: ['@commitlint/config-angular'],
   rules: {
-    'type-enum': [RuleConfigSeverity.Error, 'always', [
+    'type-enum': [2, 'always', [
       'build',
       'ci',
       'docs',
@@ -24,7 +21,7 @@ const Configuration: UserConfig = {
       // additional types
       'chore'
     ]],
-    'scope-enum': [RuleConfigSeverity.Error, 'always', [
+    'scope-enum': [2, 'always', [
       'plugin',
       'vscode',
       'git',
@@ -35,12 +32,12 @@ const Configuration: UserConfig = {
       'readme',
       'release'
     ]],
-    'scope-empty': [RuleConfigSeverity.Disabled],
+    'scope-empty': [0],
   },
   defaultIgnores: true,
   ignores: [
-    (commit: string) => commit.startsWith('build(deps): bump'),
-    (commit: string) => commit.startsWith('ci(deps): bump')
+    (commit) => commit.startsWith('build(deps): bump'),
+    (commit) => commit.startsWith('ci(deps): bump')
   ],
 };
 
